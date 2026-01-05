e9e7553ee5d5bb5800d97ec048235a30a06d10c8
gaagrind-blip <gaagrind@gmail.com>
2026-01-05 22:33:55 +0000

    Update launch_app.py

--- launch_app.py
+++ launch_app.py
@@ -3,7 +3,8 @@ import subprocess
 import sys
 
 # Get the full path of your Streamlit app
-app_path = os.path.join(os.path.dirname(__file__), "fitness_app_pro_keptstyle_plus_coachfeatures.py")
+app_path = os.path.join(os.path.dirname(__file__), "app.py")
 
 # Launch Streamlit just like using terminal
+
 subprocess.run([sys.executable, "-m", "streamlit", "run", app_path])
