844f8c93eb62d3d78504302fc0b20fc129067bae
gaagrind-blip <gaagrind@gmail.com>
2026-01-05 22:13:20 +0000

    Delete launch_app.py

--- launch_app.py
+++
@@ -1,9 +0,0 @@
-import os
-import subprocess
-import sys
-
-# Get the full path of your Streamlit app
-app_path = os.path.join(os.path.dirname(__file__), "fitness_app.py")
-
-# Launch Streamlit just like using terminal
-subprocess.run([sys.executable, "-m", "streamlit", "run", app_path])
